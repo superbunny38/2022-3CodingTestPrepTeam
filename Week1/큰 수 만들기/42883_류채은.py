a44cfcefed39cda84fb2b753047a9e3b5f813102
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2022-12-18 20:10:16 +0900

    Add files via upload

--- Week1/큰 수 만들기/42883_류채은.py
+++ Week1/큰 수 만들기/42883_류채은.py
@@ -1,50 +1,53 @@
-# 아직 틀린 답..
-
-import numpy as np
-import sys
-sys.setrecursionlimit(1000000)
-
 def solution(number, k):
-    # print(f"\n\nNUMBER: {number} pick {len(number)-k} digits\n")
-    area = list(number)
-    n_iters = len(number)-k
-    answer = ""
-    n_pick = len(number) - k
-    
-    for _ in range(n_iters):    
-        if len(area) == n_pick:
-            # print(f"adding on...{area}")
-            answer = answer + "".join(area)
-            break
-        if n_pick == 1:
-            answer += max(area)
-            break
-        no_access = area[-(n_pick-1):]
-        to_choose_from = area[:-(n_pick-1)]
-        # print(f"\nn_pick: {n_pick}")
-        # print(f"area:{area}")
-        # print(f"no access:{no_access}")
-        # print(f"choose from:{to_choose_from}")
-        picked = max(to_choose_from)
-        # print(f"picked: {picked}")
-        answer += str(picked)
-        picked_idx = area.index(picked)
-        area = area[picked_idx+1:]
-        n_pick -= 1
-
+    print(f"\n\nNUMBER: {number} k:{k}")
+    biggest_number = []
+    n_remain = len(number)-k
+    for idx, num in enumerate(number):
+        print(f"num: {num}")
+        if idx == 0:
+            biggest_number.append(num)
+            continue
+        if k ==0:
+            biggest_number.append(num)
+            continue
+        if biggest_number[-1] <= num:
+            while len(biggest_number) != 0 and biggest_number[-1] < num and k>0:
+                biggest_number.pop(-1)
+                k-=1
+            biggest_number.append(num)
+        else:
+            biggest_number.append(num)
+        print(biggest_number)
+        
+    if len(biggest_number) != n_remain:
+        biggest_number = biggest_number[:n_remain]
+    answer = "".join(biggest_number)
     return answer
-
-
 
 number = "1924"
 k = 2
-print(solution(number, k))
+print(solution(number,k))
 
 number = "1231234"
 k = 3
-
-print(solution(number, k))
+print(solution(number,k))
 
 number = "4177252841"
 k = 4
-print(solution(number, k))
+print(solution(number,k))
+
+number = "5284192"
+k = 1
+print(solution(number,k))
+
+number = "11"
+k = 1
+print(solution(number,k))
+
+number = "31"
+k = 1
+print(solution(number,k))
+
+number = "13"
+k = 1
+print(solution(number,k))
