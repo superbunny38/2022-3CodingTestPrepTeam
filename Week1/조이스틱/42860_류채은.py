92740ef112e7ff6d5ead73f90cf87150879335c1
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2022-12-19 10:20:43 +0900

    Update 42860_류채은.py

--- Week1/조이스틱/42860_류채은.py
+++ Week1/조이스틱/42860_류채은.py
@@ -1,28 +1,30 @@
-import copy
-def choose_dir(name, cur_alphabet):
-    backward = 0
-    forward = 0
-    cur_index = name.index(cur_alphabet)
-    backward_path = name[cur_index+1:]
-    forward_path = name[:cur_index]
-    
-    for letter in name:
-        if letter == 'A':
-            backward +=1
-            backward_path.remove(letter)
-        else:
-            break
-    for letter in name[::-1]:
-        if letter == 'A':
-            forward +=1
-            forward_path.remove(letter)
-        else:
-            break
-    if backward>forward:
-        return 'back', backward_path[::-1]
-    else:
-        return 'straight', forward_path
+#류채은
+#조이스틱
             
+def straight_forward(name):
+  
+    last_not_A = len(name)-1
+    for idx,alphabet in enumerate(name[::-1]):
+        if alphabet == 'A':
+            continue
+        else:
+            last_not_A = (len(name)-1)-idx
+            break
+    return last_not_A
+
+def straight_backward(name):
+
+    dest_idx = 1
+    for idx, alphabet in enumerate(name):
+        if idx == 0:
+            continue
+        if alphabet == 'A':
+            continue
+        else:
+            dest = alphabet
+            dest_idx = idx
+            break
+    return (len(name) - dest_idx)
 
 def shortest_alphabet_path(alphabet):
     db = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -33,14 +35,32 @@ def shortest_alphabet_path(alphabet):
 def solution(name):
     print(f"\n\nNAME:{name} length: {len(name)}")
     name = list(name)
+    final_cost = 0
     move_cost = 0
+    pivotting_cost= len(name)
     #alphabet cost
-    for alphabet in name:
+    for idx,alphabet in enumerate(name):
         cost = shortest_alphabet_path(alphabet)
         print(f"alphabet:{alphabet} cost: {cost}")
         final_cost += cost
+
+        #calculate pivotting
+        next_idx= idx+1
+        if next_idx<len(name) and name[next_idx] == 'A':
+            while next_idx<len(name) and name[next_idx] == 'A':
+                next_idx += 1
+            dest_idx = next_idx
+            pivotting_cost = min(pivotting_cost,2*idx+len(name)-dest_idx)
+            
         
     #move cost
+    print(f"alphabet cost: {final_cost}")
+    straight_forward_cost = straight_forward(name)
+    straight_backward_cost = straight_backward(name)
+    print(f"straight_forward_cost:{straight_forward_cost}")
+    print(f"straight_backward_cost:{straight_backward_cost}")
+    print(f"pivotting_cost:{pivotting_cost}")
+    move_cost = min(straight_forward_cost, straight_backward_cost, pivotting_cost)
     final_cost += move_cost    
     answer = final_cost
     return answer
