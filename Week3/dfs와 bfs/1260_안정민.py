36bd2e8a739fc1a3aa9a4559c104d46631c38400
AnJeongMin <ajhappy12@gmail.com>
2022-12-31 19:35:08 +0900

    solve: [안정민] 1260 - dfs와 bfs 주석 오류 발견으로 인한 주석 추가

--- Week3/dfs와 bfs/1260_안정민.py
+++ Week3/dfs와 bfs/1260_안정민.py
@@ -52,8 +52,30 @@ def bfs(start, graph, maxV) -> list:
     while dq:
         curr = dq.popleft()
         result.append(curr)
-        
+ 
         [check(x) for x in filter(lambda x: not visited[x], sorted(graph[curr]))]
+        
+        '''
+        첫 풀이는 바로 ac을 받아서 아래와 같은 코드로 다시 채점을 했는데 실패
+        [check(x) for x in sorted(filter(lambda x: not visited[x], graph[curr]))]
+        
+        - 문제점 -
+        4 3 1
+        1 2
+        1 2
+        3 4
+     
+        답: 1 2
+        위 코드: 1 2 2
+        
+        실제로는 노드 중복이 있을 수 있기 때문에 for문을 돌 때마다 해당 노드가 방문했는 지를 체크해야 함
+        
+        운이 좋게도 filter는 generator를 반환하는데, 이는 한번에 list를 반환하는게 아닌 접근?을 할 때마다 생성을 해주어
+        방문체크가 되었던 것
+        
+        조건(중복 간선)을 체크 못 하고 푼 거라 사실 상 틀림... 
+        '''
+
            
     return result
 
