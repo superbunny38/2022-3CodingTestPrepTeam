3dba1d1ecb93462ae3dc517beb4759f8f7c68876
AnJeongMin <ajhappy12@gmail.com>
2023-01-01 19:56:16 +0900

    solve: [안정민] 1463 - 1로 만들기 (dfs 풀이 추가)

--- Week3/1로 만들기/1463_안정민.py
+++ Week3/1로 만들기/1463_안정민.py
@@ -1,6 +1,9 @@
+globalCnt = int(1e9)
+
 def main():
     n = int(input())
-    count = dp(n)
+    # count = dp(n)
+    count = dfs(n, 0)
     print(count)
     
 def dp(n) -> int:
@@ -16,5 +19,23 @@ def dp(n) -> int:
             dp[i] = min(dp[i], dp[i // 2] + 1)
          
     return dp[n]
+
+def dfs(n, cnt) -> int:
+    global globalCnt
+    
+    if cnt >= globalCnt:
+        return
+    
+    if n == 1:
+        globalCnt = min(globalCnt, cnt)
+        return globalCnt
+    
+    if n % 3 == 0:
+        dfs(n // 3, cnt + 1)
+    if n % 2 == 0:
+        dfs(n // 2, cnt + 1)
+    dfs(n - 1, cnt + 1)
+
+    return globalCnt
     
 main()
