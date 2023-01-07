4765916c7dab51dcd222ec9e271034e93f2a9761
AnJeongMin <ajhappy12@gmail.com>
2023-01-07 22:41:03 +0900

    solve: 프로그래머스 - 타겟넘버 (배열 순회?)

--- Week4/타켓넘버/안정민.py
+++ Week4/타켓넘버/안정민.py
@@ -1,8 +1,11 @@
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
     # 2^20 = 1_000_000
     
-    answer = dfs(0, 0, target, numbers)
+    # answer = dfs(0, 0, target, numbers)
+    answer = traversal(numbers, target)
     return answer
 
 def dfs(idx, currSum, target, numbers):
@@ -15,3 +18,10 @@ def dfs(idx, currSum, target, numbers):
     ways += dfs(idx + 1, target, currSum + num, numbers) + dfs(idx + 1, target, currSum - num, numbers)
         
     return ways
+
+def traversal(numbers, target) -> int:
+    prevList = [0]
+    for num in numbers:
+        prevList = list(map(lambda x: x + num, prevList)) + list(map(lambda x: x - num, prevList))
+        
+    return prevList.count(target)
