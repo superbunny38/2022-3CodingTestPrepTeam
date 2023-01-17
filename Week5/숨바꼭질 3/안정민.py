1288ecd445f4593acfd9ec708094704ad00e5a9f
AnJeongMin <ajhappy12@gmail.com>
2023-01-17 22:19:32 +0900

    solve: 13539 - 숨바꼭질 3 (다익스트라 풀이)

--- Week5/숨바꼭질 3/안정민.py
+++ Week5/숨바꼭질 3/안정민.py
@@ -1,8 +1,10 @@
 from collections import deque
+import heapq
 
 def main() -> None:
     src, dst = map(int, input().split())
-    cnt = bfs(src, dst)
+    # cnt = bfs(src, dst)
+    cnt = dijkstra(src, dst)
     print(cnt)
 
 def bfs(src, dst) -> int:
@@ -24,6 +26,29 @@ def bfs(src, dst) -> int:
         dq.appendleft((now * 2, time))
         
     return -1
+
+def dijkstra(src, dst) -> int:
+    INF = int(1e9)
+    hq = []
+    times = [INF] * 200_000
+    times[src] = 0
+    heapq.heappush(hq, (0, src)) # time, node
+    while hq:
+        time, now = heapq.heappop(hq)
+        if times[now] < time:
+            continue
+        
+        candidates = (time + 1, now + 1), (time + 1, now - 1), (time, now * 2)
+        for candi in candidates:
+            t, next = candi
+            if not isRange(next):
+                continue
+            
+            if t < times[next]:
+                times[next] = t
+                heapq.heappush(hq, (t, next))
+        
+    return times[dst]
 
 def isRange(node):
     return 0 <= node < 200_000
